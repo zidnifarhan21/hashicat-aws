cba7d58d7dac20e406e61815c45cb69e5ed5142c
zidni farhan <zidnifarhan21@github.com>
2022-12-06 07:21:45 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/zidni-farhan/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket_prefix = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
